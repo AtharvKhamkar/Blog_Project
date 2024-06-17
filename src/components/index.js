fa0ff32e84fde0f400c2930a5a05805a560ac788
AtharvKhamkar <khamkaratharv2002@gmail.com>
2024-06-18 01:42:02 +0530

    add react-hook-form

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,11 @@ import Header from "./Header/Header";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
 import Container from "./container/Container";
+import Input from "./Input"
+import Login from "./Login";
+import PostCard from "./PostCard";
+import Select from "./Select"
 
-export { Button, Container, Footer, Header, Logo, LogoutBtn };
+
+export { Button, Container, Footer, Header, Logo, LogoutBtn,Input,Login,PostCard,Select};
 
